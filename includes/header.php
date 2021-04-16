945fdb0307d8d0316f5067275cb7f79c2c77f126
wil <elsalvadorazul@outlook.es>
2021-04-16 12:09:36 -0600

    este es mi punto de partida

---
+++ includes/header.php
@@ -0,0 +1,21 @@
+<?php include("db.php") ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP MY CRUD</title>
+    <!-- bosstrap 4.5 -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+</head>
+
+<nav class="navbar navbar-dark bg-dark">
+    <div class="container">
+        <a href="" class="navbar-brand">PHP MYSQL CRUD</a>
+    </div>
+</nav>
+
+<body>
---
+++ index.php
@@ -0,0 +1,13 @@
+    <?php include("db.php") ?>
+    
+    <?php include("includes/header.php") ?>
+
+   
+
+<div class="container">
+    
+</div>
+
+   <?php include("includes/footer.php")?>
+
+   
