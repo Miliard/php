0d10625397ac818a4c0a86a8decd6c304dfa31e5
wil <elsalvadorazul@outlook.es>
2021-04-16 14:14:41 -0600

    proyecto final

--- includes/header.php
+++ includes/header.php
@@ -21,3 +21,17 @@
 <body>
 
 esto es un texto de prueba para saber si esta mmierda me va ser o q pedo
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
